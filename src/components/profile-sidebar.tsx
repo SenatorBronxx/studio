505029927a21b0a0405eeabb9ead97f0a5d059ac
Francis Assuah <ynwbronxx360@gmail.com>
2025-11-12 01:30:09 +0000

    add ewe, sefwi, hausa and Kasem to the language list (_for element <Prim

--- src/components/profile-sidebar.tsx
+++ src/components/profile-sidebar.tsx
@@ -308,6 +308,10 @@ export function ProfileSidebar() {
                                 <SelectItem value="en">English</SelectItem>
                                 <SelectItem value="tw">Twi</SelectItem>
                                 <SelectItem value="ga">Ga</SelectItem>
+                                <SelectItem value="ew">Ewe</SelectItem>
+                                <SelectItem value="sf">Sefwi</SelectItem>
+                                <SelectItem value="ha">Hausa</SelectItem>
+                                <SelectItem value="ka">Kasem</SelectItem>
                             </SelectContent>
                         </Select>
                     </div>
